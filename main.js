f893c93b4f8b6f0559a54328207abfc098484a3f
Ramselove <ramselove@gmail.com>
2021-01-31 23:09:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+async function getArticles() {
+    let res = await fetch('http://localhost:8082/api.blog.ru/articles');
+    let articles = await res.json();
+
+    document.querySelector('.post-list').innerHTML = '';
+
+    articles.forEach((article) => {
+        document.querySelector('.post-list').innerHTML += `
+        <div class="card" style="width: 18rem">
+                <div class="card-body">
+                    <h5 class="card-title">${article.article_title}</h5>
+                    <p class="card-text">${article.article_content}</p>
+                    <a href="#" class="card-link">Podrobnee</a>
+                </div>
+            </div>
+        `
+    });
+};
+
+async function addArticle() {
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    let formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+
+    const res = await fetch('http://localhost:8082/api.blog.ru/articles/create', {
+        method: 'POST',
+        body: formData
+    });
+
+    const data = await res.json();
+
+    if (data.status === true) {
+        await getArticles();
+    };
+
+};
+
+getArticles();
