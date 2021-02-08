c536c22bebe5f20cb29237ff8e3ab87cea138abc
Ramseloff <ramselove@gmail.com>
2021-02-08 15:11:53 +0300

    first commit

--- main.js
+++ main.js
@@ -35,7 +35,33 @@ async function addArticle() {
     if (data.status === true) {
         await getArticles();
     };
+};
 
+async function getArticlesTag() {
+    const tag = document.getElementById('tag').value;
+    if (tag === '') {
+        await getArticles();
+    } else {
+        console.log(tag);
+        let url = 'http://localhost:8082/api.blog.ru/articles/' + tag + '/tag';
+        console.log(url);
+        let res = await fetch(url);
+        let articles = await res.json();
+
+        document.querySelector('.post-list').innerHTML = '';
+
+        articles.forEach((article) => {
+            document.querySelector('.post-list').innerHTML += `
+        <div class="card" style="width: 18rem">
+                <div class="card-body">
+                    <h5 class="card-title">${article.article_title}</h5>
+                    <p class="card-text">${article.article_content}</p>
+                    <a href="#" class="card-link">Podrobnee</a>
+                </div>
+            </div>
+        `
+        });
+    }
 };
 
 getArticles();
