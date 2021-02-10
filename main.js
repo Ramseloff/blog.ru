62ab0ccb29e631432f11980cfd6102a503b78a59
Ramseloff <ramselove@gmail.com>
2021-02-10 14:15:52 +0300

    First commit

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+/*function CheckboxCollection(checkboxes) {
+    this.checkboxes = checkboxes ? "nodeType" in checkboxes ? [checkboxes] : Array.prototype.slice.call(checkboxes) : [];
+}
+
+
+CheckboxCollection.prototype.collectValues = function () {
+    var i = this.checkboxes.length, buffer = [];
+    while (i--) {
+        if (this.checkboxes[i].checked) {
+            buffer.push(this.checkboxes[i].value);
+        }
+    }
+    return buffer;
+};
+
+
+
+*/
+
 async function getArticles() {
     let res = await fetch('http://localhost:8082/api.blog.ru/articles');
     let articles = await res.json();
@@ -12,9 +31,51 @@ async function getArticles() {
                     <p class="card-text">${article.article_content}</p>
                     <a href="#" class="card-link">Podrobnee</a>
                 </div>
+                <button class="btn btn-primary" onclick="updateArticle(${article.article_id})">edit</button>
+                <button class="btn btn-primary" onclick="deleteArticle(${article.article_id})">delete</button>
             </div>
         `
     });
+};
+
+
+async function updateArticle(id) {
+    console.log(id);
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+
+    let body = {
+        "title": title,
+        "content": content
+    };
+
+    console.log(body);
+    let url = 'http://localhost:8082/api.blog.ru/article/update/' + id;
+    const res = await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(body)
+    });
+
+    const data = await res.json();
+
+    if (data.status === true) {
+        await getArticles();
+    };
+};
+
+async function deleteArticle(id) {
+    console.log(id);
+
+    let url = 'http://localhost:8082/api.blog.ru/article/delete/' + id;
+    const res = await fetch(url, {
+        method: 'DELETE',
+    });
+
+    const data = await res.json();
+
+    if (data.status === true) {
+        await getArticles();
+    };
 };
 
 async function addArticle() {
@@ -38,13 +99,40 @@ async function addArticle() {
 };
 
 async function getArticlesTag() {
-    const tag = document.getElementById('tag').value;
+    function getCheckedCheckBoxes() {
+        var checkboxes = document.getElementsByClassName('check');
+        var checkboxesChecked = []; // можно в массиве их хранить, если нужно использовать 
+        var tag = '';
+        for (var index = 0; index < checkboxes.length; index++) {
+            if (checkboxes[index].checked) {
+                checkboxesChecked.push(checkboxes[index].value); // положим в массив выбранный
+                tag += checkboxes[index].value + '&';
+                // console.log(tag);
+            }
+        }
+        return tag; // для использования в нужном месте
+    }
+    let tag = getCheckedCheckBoxes();
+    //let tags = new CheckboxCollection();
+    /*const tag1 = document.getElementById('tag1').value;
+    const tag2 = document.getElementById('tag2').value;
+    const tag3 = document.getElementById('tag3').value;*/
+   /* tags.checkboxes.push(tag1);
+    tags.checkboxes.push(tag2);
+    tags.checkboxes.push(tag3);
+    console.log(tags);
+    tags = JSON.stringify(tags);
+    console.log(tags);*/
+    //let tag = tag1 + '&' + tag2 + '&' + tag3;
+
+    console.log(tag);
+
     if (tag === '') {
         await getArticles();
     } else {
-        console.log(tag);
-        let url = 'http://localhost:8082/api.blog.ru/articles/' + tag + '/tag';
-        console.log(url);
+       
+        let url = 'http://localhost:8082/api.blog.ru/articles/tag/' + tag;
+        // console.log(url);
         let res = await fetch(url);
         let articles = await res.json();
 
@@ -61,7 +149,7 @@ async function getArticlesTag() {
             </div>
         `
         });
-    }
+    };
 };
 
 getArticles();
